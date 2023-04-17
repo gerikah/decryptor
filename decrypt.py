7444e7f0677d765c8d589f4445b0beff4c0a1389
gerikah <aldaygerikah@gmail.com>
2023-04-17 11:48:55 +0800

    pseudocode

---
+++ decrypt.py
@@ -0,0 +1,10 @@
+# Object Oriented Programming - CMPE-103 LAB EXERCISE No. 1 : Problem 3
+
+# input from user
+# check each character
+# if * , change to a
+# if & , change to e
+# if # , change to i
+# if + , change to o
+# if ! , change to u
+# print output
