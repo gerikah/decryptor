add9d2cabbfe04dbb7d66e4ee1d12e69e93c46da
gerikah <aldaygerikah@gmail.com>
2023-04-17 12:57:00 +0800

    module import and title

--- decrypt.py
+++ decrypt.py
@@ -1,5 +1,10 @@
 # Object Oriented Programming - CMPE-103 LAB EXERCISE No. 1 : Problem 3
 
+import pyfiglet
+border = "-" * 184
+title = ("\n\n" + border + "\n\n" + "\033[95m" + pyfiglet.figlet_format("Decryption\n", justify = "center", font = "isometric1", width = 175) + "\n")
+print(title)
+
 # input from user
 # check each character
 # if * , change to a
